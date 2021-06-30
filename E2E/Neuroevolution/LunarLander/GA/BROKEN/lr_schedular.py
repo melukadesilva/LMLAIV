da6a1d38468ac02b8b7c1057f0062d4517208bc8
melukadesilva <perumadura.de@aau.at>
2021-06-30 16:02:12 +0200

    ga parallal added

---
+++ E2E/Neuroevolution/LunarLander/GA/BROKEN/lr_schedular.py
@@ -0,0 +1,20 @@
+import math
+
+class CosineAnnealingSchedule():
+    def __init__(self, min_lr, max_lr, cycle_length):
+        """
+        min_lr: lower bound for learning rate (float)
+        max_lr: upper bound for learning rate (float)
+        cycle_length: iterations between start and finish (int)
+        """
+        self.min_lr = min_lr
+        self.max_lr = max_lr
+        self.cycle_length = cycle_length
+        
+    def __call__(self, iteration):
+        if iteration <= self.cycle_length:
+            unit_cycle = (1 + math.cos(iteration * math.pi / self.cycle_length)) / 2
+            adjusted_cycle = (unit_cycle * (self.max_lr - self.min_lr)) + self.min_lr
+            return adjusted_cycle
+        else:
+            return self.min_lr
